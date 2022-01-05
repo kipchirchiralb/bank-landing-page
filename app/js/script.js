423bbdf9d279ae7d529dfc8e90c597e915858c18
kipchirchiralbert <albertkipchirchir02@gmail.com>
2022-01-05 09:30:31 +0300

    nav bar

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,27 @@
-console.log("ell wol")
+const btnHumburger = document.querySelector("#btnHumburger")
+const body = document.querySelector("body")
+const header = document.querySelector(".header")
+const overlay = document.querySelector(".overlay")
+const fadeElements = document.querySelectorAll(".has-fade")
+
+btnHumburger.addEventListener("click", () => { //close humburger manu
+    if (header.classList.contains("open")) {
+        header.classList.remove("open")
+        body.classList.remove("noscroll")
+
+        fadeElements.forEach(function(element) {
+            element.classList.remove("fade-in")
+            element.classList.add("fade-out")
+        })
+    } else { //open humberger menu
+        header.classList.add("open")
+        body.classList.add("noscroll")
+        fadeElements.forEach(function(element) {
+            element.classList.add("fade-in")
+            element.classList.remove("fade-out")
+        })
+
+    }
+
+
+})
